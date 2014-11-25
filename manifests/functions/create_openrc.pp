0709a7c7ee5387b0e516096e87d3318893f9e669
Joe Topjian <joe@topjian.net>
2014-11-25 22:16:47 +0000

    Modifying create_openrc again for keystone v3

--- manifests/functions/create_openrc.pp
+++ manifests/functions/create_openrc.pp
@@ -35,15 +35,24 @@
 #
 define cubbystack::functions::create_openrc (
   $admin_password,
-  $keystone_host    = '127.0.0.1',
-  $keystone_version = 'v2.0',
-  $admin_user       = 'admin',
-  $admin_tenant     = 'admin',
-  $region           = 'RegionOne',
-  $protocol         = 'http',
-  $owner            = 'root',
-  $group            = 'root',
+  $keystone_host        = '127.0.0.1',
+  $keystone_api_version = 'v2.0',
+  $admin_user           = 'admin',
+  $admin_tenant         = 'admin',
+  $region               = 'RegionOne',
+  $protocol             = 'http',
+  $owner                = 'root',
+  $group                = 'root',
 ) {
+
+  if $keystone_api_version == 'v3' {
+    $identity_api_version = '3'
+  } else {
+    $identity_api_vesion = '2'
+  }
+
+
+
   file { $name:
     owner   => $owner,
     group   => $group,
@@ -53,9 +62,10 @@ define cubbystack::functions::create_openrc (
 export OS_TENANT_NAME=${admin_tenant}
 export OS_USERNAME=${admin_user}
 export OS_PASSWORD=\"${admin_password}\"
-export OS_AUTH_URL=\"${protocol}://${keystone_host}:5000/${keystone_version}/\"
+export OS_AUTH_URL=\"${protocol}://${keystone_host}:5000/${keystone_api_version}/\"
 export OS_AUTH_STRATEGY=keystone
 export OS_REGION_NAME=${region}
+export OS_IDENTITY_API_VERSION=${identity_api_version}
 "
   }
 }
