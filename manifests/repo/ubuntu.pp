3a95fcb9bf48e9a2c3992eb455d56eda81199a41
Joe Topjian <joe@topjian.net>
2014-12-09 22:32:42 +0000

    Simplifying Ubuntu repo config
    This commit removes most of the logic from the repo manifest. The
    user is now expected to know what OpenStack releases are available
    for the version of Ubuntu they are using.

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -3,36 +3,12 @@ class cubbystack::repo::ubuntu (
   $repo    = 'updates'
 ) {
 
-  case $::lsbdistcodename {
-    'precise': {
-      if $release != 'juno' {
-        include apt::update
-
-        apt::source { 'ubuntu-cloud-archive':
-          location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-          release           => "${::lsbdistcodename}-${repo}/${release}",
-          repos             => 'main',
-          required_packages => 'ubuntu-cloud-keyring',
-        }
-
-        Exec['apt_update'] -> Package<||>
-      }
-
-    }
-    'trusty': {
-      if $release == 'juno' {
-        include apt::update
-
-        apt::source { 'ubuntu-cloud-archive':
-          location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-          release           => "${::lsbdistcodename}-${repo}/${release}",
-          repos             => 'main',
-          required_packages => 'ubuntu-cloud-keyring',
-        }
-
-        Exec['apt_update'] -> Package<||>
-
-      }
-    }
+  include apt::update
+  apt::source { 'ubuntu-cloud-archive':
+    location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+    release           => "${::lsbdistcodename}-${repo}/${release}",
+    repos             => 'main',
+    required_packages => 'ubuntu-cloud-keyring',
   }
+
 }
