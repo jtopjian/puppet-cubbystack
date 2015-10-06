3fd287e05f21f9819c7f586d595b264b127c8395
Cybera Sysadmin <sysadmin@cybera.ca>
2015-10-06 16:44:27 -0600

    Added repo support for ubuntu trusty

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -1,10 +1,23 @@
 class cubbystack::repo::ubuntu (
-  $release = 'havana',
+  $release = 'kilo',
   $repo    = 'updates'
 ) {
 
   case $::lsbdistcodename {
     'precise': {
+      include apt::update
+
+      apt::source { 'ubuntu-cloud-archive':
+        location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+        release           => "${::lsbdistcodename}-${repo}/${release}",
+        repos             => 'main',
+        required_packages => 'ubuntu-cloud-keyring',
+      }
+
+      Exec['apt_update'] -> Package<||>
+
+    }
+    'trusty': {
       include apt::update
 
       apt::source { 'ubuntu-cloud-archive':
