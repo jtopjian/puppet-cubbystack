1fc6a7bb2364528e28c22aaede1399595096f824
Micheal Jones <micheal.jones@cybera.ca>
2016-01-26 14:55:19 -0700

    Merge master version of ubuntu repo

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -3,32 +3,12 @@ class cubbystack::repo::ubuntu (
   $repo    = 'updates'
 ) {
 
-  case $::lsbdistcodename {
-    'precise': {
-      include apt::update
-
-      apt::source { 'ubuntu-cloud-archive':
-        location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-        release           => "${::lsbdistcodename}-${repo}/${release}",
-        repos             => 'main',
-        required_packages => 'ubuntu-cloud-keyring',
-      }
-
-      Exec['apt_update'] -> Package<||>
-
-    }
-    'trusty': {
-      include apt::update
-
-      apt::source { 'ubuntu-cloud-archive':
-        location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-        release           => "${::lsbdistcodename}-${repo}/${release}",
-        repos             => 'main',
-        required_packages => 'ubuntu-cloud-keyring',
-      }
-
-      Exec['apt_update'] -> Package<||>
-
-    }
+  include apt::update
+  apt::source { 'ubuntu-cloud-archive':
+    location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+    release           => "${::lsbdistcodename}-${repo}/${release}",
+    repos             => 'main',
+    required_packages => 'ubuntu-cloud-keyring',
   }
+
 }
