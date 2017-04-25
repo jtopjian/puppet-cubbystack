1c67aa1ee841d71b0dba2a881e035074bf22fbf4
Cybera Sysadmin <sysadmin@cybera.ca>
2017-04-25 21:48:56 +0000

    Adding exclusions for ubuntu cloud repositories

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -4,11 +4,14 @@ class cubbystack::repo::ubuntu (
 ) {
 
   include apt::update
-  apt::source { 'ubuntu-cloud-archive':
-    location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-    release           => "${::lsbdistcodename}-${repo}/${release}",
-    repos             => 'main',
-    required_packages => 'ubuntu-cloud-keyring',
+
+  if ! ($::lsbdistcodename == "trusty" and $release == "icehouse") and ! ($::lsbdistcodename == "xenial" and $release == "mitaka") {
+    apt::source { 'ubuntu-cloud-archive':
+      location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+      release           => "${::lsbdistcodename}-${repo}/${release}",
+      repos             => 'main',
+      required_packages => 'ubuntu-cloud-keyring',
+    }
   }
 
 }
