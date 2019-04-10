8b5cba43a7369d3782e64b918fd4ae73a28eeac5
Joe Topjian <joe@topjian.net>
2019-04-10 22:37:12 +0000

    cubbystack_config updates
    This commit modifies cubbystack_config to support multi-value
    (OpenStack MultiStringOpt) options. It does this by changing the
    resource's parent to openstack_config and making the appropriate changes
    to cubbystack_config worked as it did before.

--- lib/puppet/provider/cubbystack_config/ini_setting.rb
+++ lib/puppet/provider/cubbystack_config/ini_setting.rb
@@ -1,7 +1,23 @@
+require 'facter'
+
 Puppet::Type.type(:cubbystack_config).provide(
-  :ini_setting,
-  :parent => Puppet::Type.type(:ini_setting).provider(:ruby)
+  :openstackconfig,
+  :parent => Puppet::Type.type(:openstack_config).provider(:ruby)
 ) do
+
+  def exists?
+    immutable_string = Facter.value(:os_immutable) || '<_IMMUTABLE_>'
+    if resource[:value][0].to_s[0..1] == '--'
+      resource[:ensure] = :absent
+    elsif resource[:value] == immutable_string or resource[:value] == [immutable_string]
+      resource[:value] = value
+      # when the value is undefined, we keep it that way.
+      if value.nil? or (value.kind_of?(Array) and value[0].nil?)
+        resource[:ensure] = :absent
+      end
+    end
+    super
+  end
 
   def section
     resource[:name].split(/\s*:\s*/, 2).last.split('/', 2).first
@@ -23,4 +39,14 @@ Puppet::Type.type(:cubbystack_config).provide(
     file_path
   end
 
+  def value
+    val = super
+    if val.length > 1
+      val
+    elsif val.length == 1
+      val[0]
+    else
+      nil
+    end
+  end
 end
--- lib/puppet/type/cubbystack_config.rb
+++ lib/puppet/type/cubbystack_config.rb
@@ -7,17 +7,52 @@ Puppet::Type.newtype(:cubbystack_config) do
     newvalues(/\S+: \S+\/\S+/)
   end
 
-  newproperty(:value) do
+  newproperty(:value, :array_matching => :all) do
     desc 'The value of the setting to be defined. If the value starts with
       "--" then ensure will be set to absent.'
-    validate do |value|
-      @resource[:ensure] = :absent if value.to_s[0..1] == '--'
+
+    def insync?(is)
+      return true if @should.empty?
+
+      if @should.is_a? Array
+        if not is.is_a? Array
+          if @should.length > 1
+            return false
+          end
+
+          return true if @should[0] == is
+        end
+      end
+
+      if @should.is_a? Array and is.is_a? Array
+        return (
+          is & @should == is or
+          is & @should.map(&:to_s) == is
+        )
+      end
+
+      return false
     end
+
     munge do |value|
       value = value.to_s.strip
       value.capitalize! if value =~ /^(true|false)$/i
       value
     end
   end
+
+  newparam(:secret, :boolean => true) do
+    desc 'Whether to hide the value from Puppet logs. Defaults to `false`.'
+
+    newvalues(:true, :false)
+
+    defaultto false
+  end
+
+  newparam(:ensure_absent_val) do
+    desc 'A value that is specified as the value property will behave as if ensure => absent was specified'
+    nil
+  end
+
 
 end
